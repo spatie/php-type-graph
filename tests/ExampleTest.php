9f5bbbaac6ad8b5d45b7131c90c0e61d0436f7f3
Ruben Van Assche <rubenvanassche@gmail.com>
2022-08-25 10:39:18 +0200

    Initial commit

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,14 @@
 <?php
 
+use Spatie\PhpTypeGraph\Actions\GenerateTypeGraphAction;
+use Spatie\PhpTypeGraph\Actions\ResolveInvertedClassReferenceMapAction;
+use Spatie\PhpTypeGraph\Actions\TraverseNodesCollectionAction;
+
 it('can test', function () {
-    expect(true)->toBeTrue();
+    $action = new GenerateTypeGraphAction(
+        new ResolveInvertedClassReferenceMapAction(),
+        new TraverseNodesCollectionAction(),
+    );
+
+    ray($action->execute()[Spatie\PhpTypeGraph\Nodes\TypeNode::class]);
 });
